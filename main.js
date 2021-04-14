0d349513e65bb22e7fd7481c2d75508e3c92191c
youngcitrus <mrjustinnguyen@gmail.com>
2021-04-14 11:09:31 -0700

    lecture_code_sqlize

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { sequelize } = require("./models");
+const { sequelize, User } = require("./models");
 
 async function main() {
   try {
@@ -11,7 +11,14 @@ async function main() {
 
   console.log("Database connection success!");
   console.log("Sequelize is ready to use!");
-
+  
+  const user = await User.findByPk(1);
+  console.log(user.toJSON());
+  console.log(user.username);
+  user.username = 'banana';
+  console.log(user.username);
+  await user.save();
+  
   // Close database connection when done with it.
   await sequelize.close();
 }
