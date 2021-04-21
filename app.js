d994b4d17761cc77bba7e3fbc08caf9bde78dc71
youngcitrus <mrjustinnguyen@gmail.com>
2021-04-21 08:48:05 -0700

    more pug template fun

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const { User } = require('./models/index');
 const usersRouter = require('./routes/users');
+const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'assets')));
 
